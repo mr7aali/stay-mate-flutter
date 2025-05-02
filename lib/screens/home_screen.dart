7bdbbe9b69f8811054b252cdd78e28db63e0aa5f
Aali <mr7aali@gmail.com>
2025-05-02 15:01:21 +0600

    bottom Navigation bar done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_app/theme/theme.dart';
 import 'package:flutter_app/widgets/custom_card.dart';
 import 'package:flutter_app/widgets/custom_drawer.dart';
 import 'package:provider/provider.dart';
@@ -64,7 +63,8 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       drawer: CustomDrawer(),
       body: Container(
-        color: Colors.blueAccent,
+        // color: Colors.blueAccent,
+        color: Colors.white70,
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 12),
 
@@ -81,30 +81,48 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               const SizedBox(height: 20),
+
               Expanded(
-                child: ListView.builder(
-                  itemCount: 30,
-                  itemBuilder: (context, index) => CustomCard(index: index),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: ListView.builder(
+                    itemCount: 30,
+                    itemBuilder: (context, index) => CustomCard(index: index),
+                  ),
                 ),
               ),
             ],
           ),
         ),
       ),
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.transparent,
-        items: const [
-          Icon(Icons.home),
-          Icon(Icons.manage_search_rounded),
-          Icon(Icons.bookmark_add_outlined),
-          Icon(Icons.person_3),
-        ],
-        index: 0,
-        animationCurve: Curves.easeInOut,
-        animationDuration: const Duration(milliseconds: 300),
-        onTap: (index) {
-          // Handle navigation
-        },
+      bottomNavigationBar: Container(
+        // evaluation: false,
+        decoration: BoxDecoration(
+          // color: Colors.black,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 20,
+              offset: const Offset(0, 10),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: CurvedNavigationBar(
+          backgroundColor: Colors.transparent,
+          items: const [
+            Icon(Icons.home),
+            Icon(Icons.manage_search_rounded),
+            Icon(Icons.bookmark_add_outlined),
+            Icon(Icons.person_3),
+          ],
+          index: 0,
+          animationCurve: Curves.easeInOut,
+          animationDuration: const Duration(milliseconds: 300),
+          onTap: (index) {
+            // Handle navigation
+          },
+        ),
       ),
     );
   }
--- lib/widgets/custom_card.dart
+++ lib/widgets/custom_card.dart
@@ -12,60 +12,57 @@ class CustomCard extends StatelessWidget {
         side: const BorderSide(color: Colors.black12, width: 1),
       ),
       elevation: 1,
-      // color: Color(0x6B81CCFF),
+
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
         child: Row(
           children: [
             Expanded(
               flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.all(0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Mountain Lodge',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Mountain Lodge',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  // const SizedBox(height: 8),
+                  const Text(
+                    'Located in Aspen, Colorado',
+                    style: TextStyle(fontSize: 14, color: Colors.black54),
+                  ),
+                  // const SizedBox(height: 4),
+                  const Text(
+                    'Ski-in/ski-out access',
+                    style: TextStyle(fontSize: 14, color: Colors.black54),
+                  ),
+                  const SizedBox(height: 12),
+                  OutlinedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Viewing Details')),
+                      );
+                    },
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(color: Colors.blue),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 8,
                       ),
                     ),
-                    const SizedBox(height: 8),
-                    const Text(
-                      'Located in Aspen, Colorado',
-                      style: TextStyle(fontSize: 14, color: Colors.black54),
+                    child: const Text(
+                      'View Details',
+                      style: TextStyle(fontSize: 14, color: Colors.blue),
                     ),
-                    const SizedBox(height: 4),
-                    const Text(
-                      'Ski-in/ski-out access',
-                      style: TextStyle(fontSize: 14, color: Colors.black54),
-                    ),
-                    const SizedBox(height: 16),
-                    OutlinedButton(
-                      onPressed: () {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Viewing Details')),
-                        );
-                      },
-                      style: OutlinedButton.styleFrom(
-                        side: const BorderSide(color: Colors.blue),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 8,
-                        ),
-                      ),
-                      child: const Text(
-                        'View Details',
-                        style: TextStyle(fontSize: 14, color: Colors.blue),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
             Expanded(
@@ -75,7 +72,7 @@ class CustomCard extends StatelessWidget {
                 borderRadius: const BorderRadius.all(Radius.circular(8)),
                 child: Image.network(
                   'https://cdn.pixabay.com/photo/2015/04/23/22/00/new-year-background-736885_1280.jpg',
-                  height: 150,
+                  height: 120,
                   width: double.infinity,
                   fit: BoxFit.cover,
                 ),
